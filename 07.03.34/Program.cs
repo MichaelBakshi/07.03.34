a479c255e7295d7858b33815311ca4afc8a4d1f7
Michael Bakshi <michael.bakshi83@gmail.com>
2021-03-11 19:56:33 +0200

    Add project files.

---
+++ 07.03.34/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._03._34
+{
+    class Program
+    {
+
+        public class Workers 
+        {
+            List<Task> five_tasks = new List<Task>(5);
+            public void MultiplyX2 (List<int> list)
+            {
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    five_tasks[i] = new Task();
+                    five_tasks[i] = Task.Run(() =>
+                     {
+                         //list[i] *= 2;
+                     });
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 3, 2 };
+            Workers workers1 = new Workers();
+            workers1.MultiplyX2(numbers);
+        }
+    }
+}
